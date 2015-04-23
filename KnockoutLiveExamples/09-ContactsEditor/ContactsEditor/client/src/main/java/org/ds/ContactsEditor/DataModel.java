ab240f8063e6b865a6092adde65a1c1ecd4dd9ae
eppleton <info@eppleton.de>
2015-04-23 18:21:02 +0200

    fixed save and added a bit css

--- KnockoutLiveExamples/09-ContactsEditor/ContactsEditor/client/src/main/java/org/ds/ContactsEditor/DataModel.java
+++ KnockoutLiveExamples/09-ContactsEditor/ContactsEditor/client/src/main/java/org/ds/ContactsEditor/DataModel.java
@@ -5,22 +5,29 @@ import net.java.html.json.Model;
 import net.java.html.json.Property;
 import org.netbeans.html.json.impl.JSON;
 
-@Model(className = "Data", targetId="", properties = {
+@Model(className = "Data", targetId = "", properties = {
     @Property(name = "contacts", type = Contact.class, array = true),
     @Property(name = "lastSavedJson", type = String.class)
 })
 final class DataModel {
-    @Function static void addContact(Data model) {
-        model.getContacts().add(new ContactData("", "", new PhoneData("","")));
-    }
-    @Function static void removeContact(Data model, ContactData data) {
-        model.getContacts().remove(data);
-    }
-    @Function static void addPhone(ContactData data) {
-        data.getPhones().add(new PhoneData("",""));
+
+    @Function
+    static void addContact(Data model) {
+        model.getContacts().add(new ContactData("", "", new PhoneData("", "")));
     }
 
-    @Function static void save(Data model) {
-        // ???
+    @Function
+    static void removeContact(Data model, ContactData data) {
+        model.getContacts().remove(data);
+    }
+
+    @Function
+    static void addPhone(ContactData data) {
+        data.getPhones().add(new PhoneData("", ""));
+    }
+    
+    @Function
+    static void save(Data model) {
+        model.setLastSavedJson(model.toString());
     }
 }
